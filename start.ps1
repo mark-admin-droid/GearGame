c8706a923d5641ce9a67234e54791ce0653c2ac8
mark-admin-droid <mark.cherniavskyi@gmail.com>
2026-07-08 23:22:41 +0100

    Updated caves

--- start.ps1
+++ start.ps1
@@ -1,4 +1,4 @@
 $scriptRoot = $PSScriptRoot
 
 Start-Process -FilePath "python" -ArgumentList @("-m", "http.server", "8080") -WorkingDirectory $scriptRoot
-Start-Process "http://localhost:8080/index.html"
+Start-Process "http://localhost:8080/index.html?fresh=1"
